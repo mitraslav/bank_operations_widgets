3a21bc5cdd3daafb5b9561f9406e1d265b5ff64a
slavaekim <alex.alphaim@gmail.com>
2025-07-02 18:27:21 +0300

    widget.py modified with isort

--- src/widget.py
+++ src/widget.py
@@ -1,5 +1,6 @@
 from masks import get_mask_account, get_mask_card_number
 
+
 def mask_account_card(user_data : str) -> str:
     if 'Счет' in user_data:
         return get_mask_account(int(user_data[user_data.find(' ')+1:]))
