cb5e7f1f037db6f7f8fd03eb7e772c077dd47aa6
slavaekim <alex.alphaim@gmail.com>
2025-07-02 18:25:27 +0300

    get_date created

--- src/widget.py
+++ src/widget.py
@@ -5,3 +5,6 @@ def mask_account_card(user_data : str) -> str:
         return get_mask_account(int(user_data[user_data.find(' ')+1:]))
     return get_mask_card_number(int(user_data[user_data.find(' ')+1:]))
 
+def get_date(date_info : str) -> str:
+    return f'{date_info[8:10]}.{date_info[5:7]}.{date_info[:4]}'
+
