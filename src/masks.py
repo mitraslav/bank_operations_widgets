ad88c1f991ca10331acfce99d87116f452fddae6
slavaekim <alex.alphaim@gmail.com>
2025-07-02 18:10:00 +0300

    changed masks.py

--- src/masks.py
+++ src/masks.py
@@ -4,7 +4,7 @@ def get_mask_card_number(card_number: int) -> str:
     return f"{card_number_str[:4]} {card_number_str[4:6]}** **** {card_number_str[-4:]}"
 
 
-def get_mask_account(card_number: int) -> str:
+def get_mask_account(account_number: int) -> str:
     """Функция принимает номер карты и возвращает маску номера по правилу **XXXX"""
-    card_number_str = str(card_number)
-    return f"**{card_number_str[-4:]}"
+    account_number_str = str(account_number)
+    return f"**{account_number_str[-4:]}"
