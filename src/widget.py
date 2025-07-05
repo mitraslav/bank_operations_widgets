814bec0dffaa738b6aafa27531c64b8317e0926b
slavaekim <alex.alphaim@gmail.com>
2025-07-05 13:56:36 +0300

    fixed mask_account_card function

--- src/widget.py
+++ src/widget.py
@@ -3,9 +3,11 @@ from src.masks import get_mask_account, get_mask_card_number
 
 def mask_account_card(user_data: str) -> str:
     """Функция маскирует счета и карты пользователя"""
-    if "Счет" in user_data:
-        return get_mask_account(int(user_data[user_data.find(" ") + 1 :]))
-    return get_mask_card_number(int(user_data[user_data.find(" ") + 1 :]))
+    user_data_list = user_data.split()
+    numbers_from_data = int(user_data_list[-1])
+    if "Счет" in user_data_list:
+        return get_mask_account(numbers_from_data)
+    return get_mask_card_number(numbers_from_data)
 
 
 def get_date(date_info: str) -> str:
