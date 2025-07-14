64e9ecb9b9a025945ea8fb305e362dc8943e7315
slavaekim <dmitryraskoln@gmail.com>
2025-07-14 11:03:54 +0300

    isort fixes

--- src/processing.py
+++ src/processing.py
@@ -1,4 +1,4 @@
-from typing import Dict, List, Any
+from typing import Any, Dict, List
 
 
 def filter_by_state(operations_list: List[Dict[str, Any]], state: str = "EXECUTED") -> List[Dict[str, Any]]:
