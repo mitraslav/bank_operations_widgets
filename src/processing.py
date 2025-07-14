d63d353af7620c4e5fbea14131c7c018510fa511
slavaekim <dmitryraskoln@gmail.com>
2025-07-14 11:02:00 +0300

    processing.py fixed

--- src/processing.py
+++ src/processing.py
@@ -1,11 +1,11 @@
-from typing import Dict, List
+from typing import Dict, List, Any
 
 
-def filter_by_state(operations_list: List[Dict], state: str = "EXECUTED") -> List[Dict]:
+def filter_by_state(operations_list: List[Dict[str, Any]], state: str = "EXECUTED") -> List[Dict[str, Any]]:
     """Функция возвращает список со словарями, у которых ключ 'state' соответствует указанному значению"""
     return [i for i in operations_list if i["state"] == state]
 
 
-def sort_by_date(operations_list: List[Dict], descending: bool = True) -> List[Dict]:
+def sort_by_date(operations_list: List[Dict[str, Any]], descending: bool = True) -> List[Dict[str, Any]]:
     """Функция сортирует список словарей по дате"""
     return sorted(operations_list, key=lambda x: x["date"], reverse=descending)
