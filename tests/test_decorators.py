557e70acbc2285dc911dead163eac89074d2d7c9
slavaekim <dmitryraskoln@gmail.com>
2025-07-31 21:34:16 +0300

    README.md supplemented and code coverage HTML created

--- tests/test_decorators.py
+++ tests/test_decorators.py
@@ -4,6 +4,7 @@ from src.decorators import log
 def test_log_stdout_check(capsys):
     @log()
     def add(a, b):
+        """Складывает два числа"""
         return a + b
 
     add(2, 3)
@@ -22,6 +23,7 @@ def test_log_to_file(tmp_path):
 
     @log(str(log_file))
     def multiply(a, b):
+        """Перемножает два числа"""
         return a * b
 
     multiply(2, 7)
@@ -34,6 +36,7 @@ def test_log_to_file(tmp_path):
 def test_log_value_error_printing(capsys):
     @log()
     def error_function():
+        """Вызывает ошибку ValueError"""
         raise ValueError
 
     error_function()
@@ -48,11 +51,12 @@ def test_log_index_error_in_file(tmp_path):
     log_file = tmp_path / "error_text.txt"
 
     @log(str(log_file))
-    def index_error_function(ind):
+    def get_slice_from_add(ind):
+        """Возвращает заданный срез слова 'add'"""
         return "add"[ind]
 
-    index_error_function(5)
+    get_slice_from_add(5)
 
     assert log_file.exists()
     content = log_file.read_text(encoding="utf-8")
-    assert "index_error_function error: IndexError. Inputs: (5,), {}" in content
+    assert "get_slice_from_add error: IndexError. Inputs: (5,), {}" in content
